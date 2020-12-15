3d5525bfbbb4a4506bc98cc908080be8b9e2c37e
Yara Oliveira <yarasereiadocerrado@gmail.com>
2020-12-14 23:42:43 -0300

    Arquivo principal do projeto

---
+++ src/main/java/com/uol/compasso/yara/SpringBootApplicationYara.java
@@ -0,0 +1,11 @@
+package com.uol.compasso.yara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationYara {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationYara.class, args);
+    }
+}
