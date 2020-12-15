fdd01c638274e5bb50680284233af93baee9cb6e
Yara Oliveira <yarasereiadocerrado@gmail.com>
2020-12-14 23:32:46 -0300

    (feature) * Inserido enumerador estado

---
+++ src/main/java/com/uol/compasso/yara/enuns/EnumEstado.java
@@ -0,0 +1,14 @@
+package com.uol.compasso.yara.enuns;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+public enum EnumEstado {
+    AC("AC"), AL("AL"), AM("AM"), AP("AP"), BA("BA"), CE("CE"), DF("DF"), ES("ES"), GO("GO"),
+    MA("MA"), MG("MG"), MS("MS"), MT("MT"), PA("PA"), PB("PB"), PE("PE"), PI("PI"), PR("PR"),
+    RJ("RJ"), RN("RN"), RO("RO"), RR("RR"), RS("RS"), SC("SC"), SE("SE"), SP("SP"), TO("TO");
+
+    @Getter
+    private final String descricao;
+   }
