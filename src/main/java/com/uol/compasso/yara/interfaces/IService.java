1e16a1d58599914bf59a9860ccadeb67c4faca69
Yara Oliveira <yarasereiadocerrado@gmail.com>
2020-12-14 23:35:05 -0300

    (feature) * Inserida interfaces do projeto

---
+++ src/main/java/com/uol/compasso/yara/interfaces/IService.java
@@ -0,0 +1,28 @@
+package com.uol.compasso.yara.interfaces;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ *
+ * @param <T>
+ * @param <N>
+ */
+public interface IService<T,N> {
+
+    T create(T entity);
+
+    List<T> read();
+
+    T read(N id);
+
+    //Page<T> read(String nome, Pageable pageable);
+
+    void update(T entity);
+
+    void deleteById(N id);
+
+    void delete(T entity);
+}
